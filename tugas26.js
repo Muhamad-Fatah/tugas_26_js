bd60b0db5035f8d63c9ff54bd248de3d483728a2
Muhamad Fatah <muhamad.fatah494@gmail.com>
2022-05-23 10:00:34 +0700

    tugas 26 js
    tugas 26 js - niomic

---
+++ tugas26.js
@@ -0,0 +1,40 @@
+function angka(x) {
+    if (isFinite(x)) {
+        return y,"NOT Infinity.";
+    }
+    return "Infinity."
+}
+
+var y = "2";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "39";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "76";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "50";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "9";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "7";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "41";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "2";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "24";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "1";
+var z = angka(y);
+console.log("Angka",y,z);
+var y = "16";
+var z = angka(y);
+console.log("Angka",y,z);
